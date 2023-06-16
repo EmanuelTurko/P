a0b89726a0bc5044260a8e3d35571bd5d4d9b517
EmanuelTurko <berserkerdefile@gmail.com>
2023-06-16 15:19:09 +0300

    1

--- common.js
+++ common.js
@@ -10,7 +10,7 @@ function updateNavbar() {
   
       // Set the logged-in username in the navbar
       var username = sessionStorage.getItem("username"); // Assuming you store the username in session storage
-      loggedInUsernameElement.textContent = "Welcome back, " + username; // Display the welcome message with the username
+      loggedInUsernameElement.textContent = "Welcome back!"; // Display the welcome message with the username
     } else {
       loginLink.style.display = "block"; // Show the login link
       registerLink.style.display = "block"; // Show the register link
