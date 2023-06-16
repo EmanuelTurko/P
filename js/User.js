b20126e22945b705199d3ba0a31241e3e6050fa8
EmanuelTurko <berserkerdefile@gmail.com>
2023-06-16 11:18:37 +0300

    update

--- js/User.js
+++ js/User.js
@@ -1,38 +1,48 @@
-
-function validatePassword() {
-    var passwordInput = document.getElementById("password");
-    var passwordError = document.getElementById("passwordError");
-
-    if (passwordInput.value.length < 6) {
-        passwordInput.classList.add("invalid");
-        passwordError.textContent = "Password must be at least 6 characters long";
-    } else {
-        passwordInput.classList.remove("invalid");
-        passwordError.textContent = "";
-    }
-}
-
 function validateForm() {
     var usernameInput = document.getElementById("username");
     var passwordInput = document.getElementById("password");
-
+  
+    // Perform client-side form validation
     if (!usernameInput.value.match(/^[a-zA-Z]+$/)) {
-        usernameInput.classList.add("invalid");
-        document.getElementById("usernameError").textContent = "Username must include only letters";
-        return false;
+      usernameInput.classList.add("invalid");
+      document.getElementById("usernameError").textContent = "Username must include only letters";
+      return false;
     } else {
-        usernameInput.classList.remove("invalid");
-        document.getElementById("usernameError").textContent = "";
+      usernameInput.classList.remove("invalid");
+      document.getElementById("usernameError").textContent = "";
     }
-
+  
     if (passwordInput.value.length < 6) {
-        passwordInput.classList.add("invalid");
-        document.getElementById("passwordError").textContent = "Password must be at least 6 characters long";
-        return false;
+      passwordInput.classList.add("invalid");
+      document.getElementById("passwordError").textContent = "Password must be at least 6 characters long";
+      return false;
     } else {
-        passwordInput.classList.remove("invalid");
-        document.getElementById("passwordError").textContent = "";
+      passwordInput.classList.remove("invalid");
+      document.getElementById("passwordError").textContent = "";
     }
-
-    return true;
-}
+  
+    // Create an object with the registration data
+    var registrationData = {
+      username: usernameInput.value,
+      password: passwordInput.value
+    };
+  
+    // Send the registration data to the server
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "/register", true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        // Registration successful
+        console.log("User registered successfully");
+        // You can redirect the user to a success page or perform any other action here
+      } else if (xhr.readyState === 4 && xhr.status !== 200) {
+        // Registration failed
+        console.error("Error registering user");
+        // You can display an error message to the user or perform any other action here
+      }
+    };
+    xhr.send(JSON.stringify(registrationData));
+  
+    return false; // Prevent the form from submitting
+  }
--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
 const app = express();
 const port = 3000;
 const path = require('path');
 
 // Connect to MongoDB
 mongoose
-  .connect('mongodb://localhost:27017', { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect('mongodb://localhost:27017/Customer', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('Connected to MongoDB');
   })
@@ -14,12 +16,45 @@ mongoose
     console.error('Error connecting to MongoDB:', error);
   });
 
+// Define a schema for the "register" collection
+const registerSchema = new mongoose.Schema({
+  username: String,
+  password: String
+});
+
+// Create a model for the "register" collection
+const Register = mongoose.model('register', registerSchema);
+
+// Parse URL-encoded bodies
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Parse JSON bodies
+app.use(bodyParser.json());
+
 // Serve static files from the 'public' directory
 app.use(express.static(path.resolve(__dirname, 'C:\\Users\\Berse\\.vscode\\Matala\\IntApps\\P\\P')));
 
 // Set the default route to redirect to index.html
 app.get('/', (req, res) => {
   res.redirect('/index.html');
+});
+
+// Handle registration form submission
+app.post('/register', (req, res) => {
+  const { username, password } = req.body;
+
+  // Create a new document using the Register model
+  const newUser = new Register({ username, password });
+
+  // Save the new user document to the "register" collection
+  newUser.save()
+    .then(() => {
+      res.send('Registration successful');
+    })
+    .catch((error) => {
+      console.error('Error registering user:', error);
+      res.status(500).send('Registration failed');
+    });
 });
 
 // Start the server
